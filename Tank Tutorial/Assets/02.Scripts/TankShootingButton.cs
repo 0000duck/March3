056ec9a0a2cafd8f94dc827928c7c3b603ec39a8
edonhi <edonhi1@gmail.com>
2018-03-21 17:59:57 +0900

    슈팅버튼 스위치구현

---
+++ Tank Tutorial/Assets/02.Scripts/TankShootingButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShootingButton : MonoBehaviour {
+
+    public bool m_Fired = true;
+
+    private void Start()
+    {
+        m_Fired = true;
+    }
+
+    void OnPress()
+    {
+        switch (m_Fired)
+        {
+            case true:
+                m_Fired = false;
+                Debug.Log("Press!");
+                break;
+            case false:
+                m_Fired = true;
+                Debug.Log("Fire!");
+                break;            
+        }  
+    }
+
+    private void Update()
+    {
+        if (!m_Fired)
+        {
+            Debug.Log("Charging!");
+        }
+    }
+}
